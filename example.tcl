4596f304c273ed7cd57658974057eb4599e62369
Neofytos Dimitriou <neo@jerily.cy>
2023-08-03 18:05:38 -0400

    separate s3 client create command

--- example.tcl
+++ example.tcl
@@ -1,3 +1,6 @@
 set dir [file dirname [dict get [info frame 0] file]]
 load [file join $dir build libaws-sdk-tcl-s3.so] Aws_sdk_tcl_s3
-puts [::aws::s3::ls some_handle some-bucket]
+
+set config_dict [dict create]
+set s3_client [::aws::s3::create config_dict]
+puts [::aws::s3::ls $s3_client "bucket" "key"]
--- src/aws-sdk-tcl-s3/library.cc
+++ src/aws-sdk-tcl-s3/library.cc
@@ -3,6 +3,7 @@
 #include <aws/s3/S3Client.h>
 #include <aws/s3/model/ListObjectsRequest.h>
 #include <aws/s3/model/Object.h>
+#include <cstdio>
 #include "library.h"
 
 #ifdef DEBUG
@@ -17,11 +18,13 @@
                      return TCL_ERROR; \
                  }
 
+#define CMD_NAME(s,internal) std::sprintf((s), "_AWS_S3_%p", (internal))
+
 static Tcl_HashTable aws_sdk_tcl_s3_NameToInternal_HT;
 static Tcl_Mutex     aws_sdk_tcl_s3_NameToInternal_HT_Mutex;
 static int           aws_sdk_tcl_s3_ModuleInitialized;
 static int
-aws_sdk_tcl_s3_RegisterName(const char *name, struct bert_ctx *ctx) {
+aws_sdk_tcl_s3_RegisterName(const char *name, Aws::S3::S3Client *internal) {
 
     Tcl_HashEntry *entryPtr;
     int newEntry;
@@ -29,47 +32,65 @@ aws_sdk_tcl_s3_RegisterName(const char *name, struct bert_ctx *ctx) {
     Tcl_MutexLock(&aws_sdk_tcl_s3_NameToInternal_HT_Mutex);
     entryPtr = Tcl_CreateHashEntry(&aws_sdk_tcl_s3_NameToInternal_HT, (char*) name, &newEntry);
     if (newEntry) {
-        Tcl_SetHashValue(entryPtr, (ClientData)ctx);
+        Tcl_SetHashValue(entryPtr, (ClientData)internal);
     }
     Tcl_MutexUnlock(&aws_sdk_tcl_s3_NameToInternal_HT_Mutex);
 
-    DBG(fprintf(stderr, "--> RegisterName: name=%s ctx=%p %s\n", name, ctx, newEntry ? "entered into" : "already in"));
+    DBG(fprintf(stderr, "--> RegisterName: name=%s internal=%p %s\n", name, internal, newEntry ? "entered into" : "already in"));
 
     return 0;
 }
 
-static struct bert_ctx *
+static struct Aws::S3::S3Client *
 aws_sdk_tcl_s3_GetInternalFromName(const char *name) {
-    struct bert_ctx *ctx = NULL;
+    Aws::S3::S3Client *internal = NULL;
     Tcl_HashEntry *entryPtr;
 
     Tcl_MutexLock(&aws_sdk_tcl_s3_NameToInternal_HT_Mutex);
     entryPtr = Tcl_FindHashEntry(&aws_sdk_tcl_s3_NameToInternal_HT, (char*)name);
     if (entryPtr != NULL) {
-        ctx = (struct bert_ctx *)Tcl_GetHashValue(entryPtr);
+        internal = (Aws::S3::S3Client *)Tcl_GetHashValue(entryPtr);
     }
     Tcl_MutexUnlock(&aws_sdk_tcl_s3_NameToInternal_HT_Mutex);
 
-    return ctx;
+    return internal;
+}
+
+static int aws_sdk_tcl_s3_CreateCmd(ClientData  clientData, Tcl_Interp *interp, int objc, Tcl_Obj * const objv[] ) {
+    DBG(fprintf(stderr, "CreateCmd\n"));
+
+    CheckArgs(2,2,1,"config_dict");
+
+    Aws::Client::ClientConfiguration clientConfig;
+    // Optional: Set to the AWS Region (overrides config file).
+    // clientConfig.region = "us-east-1";
+
+//    Aws::S3::S3Client s3Client(clientConfig);
+    Aws::S3::S3Client *s3Client = new Aws::S3::S3Client(clientConfig);
+    char handle[80];
+    CMD_NAME(handle, s3Client);
+    aws_sdk_tcl_s3_RegisterName(handle, s3Client);
+    Tcl_SetObjResult(interp, Tcl_NewStringObj(handle, -1));
+    return TCL_OK;
 }
 
 static int aws_sdk_tcl_s3_ListCmd(ClientData  clientData, Tcl_Interp *interp, int objc, Tcl_Obj * const objv[] ) {
     DBG(fprintf(stderr,"ListCmd\n"));
 
-    CheckArgs(3,3,1,"handle_name bucket");
+    CheckArgs(3,4,1,"handle_name bucket ?key?");
 
-    const Aws::String bucketName = Aws::String(Tcl_GetString(objv[2]));
+    const char *handleName = Tcl_GetString(objv[1]);
+    const Aws::String bucketName = Tcl_GetString(objv[2]);
 
-    Aws::Client::ClientConfiguration clientConfig;
-        // Optional: Set to the AWS Region (overrides config file).
-        // clientConfig.region = "us-east-1";
-
-    Aws::S3::S3Client s3_client(clientConfig);
-
+    Aws::S3::S3Client *s3Client = aws_sdk_tcl_s3_GetInternalFromName(handleName);
     Aws::S3::Model::ListObjectsRequest request;
     request.WithBucket(bucketName);
+    if (objc == 4) {
+        const Aws::String keyName = Tcl_GetString(objv[3]);
+        request.WithPrefix(keyName);
+    }
 
-    auto outcome = s3_client.ListObjects(request);
+    auto outcome = s3Client->ListObjects(request);
 
     if (!outcome.IsSuccess()) {
         std::cerr << "Error: ListObjects: " <<
@@ -118,6 +139,7 @@ int Aws_sdk_tcl_s3_Init(Tcl_Interp *interp) {
     aws_sdk_tcl_s3_InitModule();
 
     Tcl_CreateNamespace(interp, "::aws::s3", NULL, NULL);
+    Tcl_CreateObjCommand(interp, "::aws::s3::create", aws_sdk_tcl_s3_CreateCmd, NULL, NULL);
     Tcl_CreateObjCommand(interp, "::aws::s3::ls", aws_sdk_tcl_s3_ListCmd, NULL, NULL);
 
     return Tcl_PkgProvide(interp, "aws_sdk_tcl_s3", "0.1");
