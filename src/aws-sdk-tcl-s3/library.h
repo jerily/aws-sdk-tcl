5f7e4cb26c36c979967533c09e85ead89a586df2
Neofytos Dimitriou <neo@jerily.cy>
2023-08-16 11:01:42 -0400

    added SPDX-FileCopyrightText

--- src/aws-sdk-tcl-s3/library.h
+++ src/aws-sdk-tcl-s3/library.h
@@ -1,5 +1,6 @@
 /**
  * Copyright Jerily LTD. All Rights Reserved.
+ * SPDX-FileCopyrightText: 2023 Neofytos Dimitriou (neo@jerily.cy)
  * SPDX-License-Identifier: MIT.
  */
 #ifndef AWS_SDK_TCL_S3_LIBRARY_H
