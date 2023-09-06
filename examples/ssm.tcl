608d1c749fe8c497367a22421557981852037ba7
Neofytos Dimitriou <neo@jerily.cy>
2023-09-06 01:47:40 -0400

    fixed example

--- examples/ssm.tcl
+++ examples/ssm.tcl
@@ -1,6 +1,7 @@
 package require awsssm
 
-set client [::aws::ssm::create [dict create]]
+set config_dict [dict create region us-east-1 endpoint "http://localhost:4566"]
+set client [::aws::ssm::create $config_dict]
 $client put_parameter "/test/parameter" "test value" "String"
 puts test_parameter=[$client get_parameter "/test/parameter"]
 #$client delete_parameter "/test/parameter"
